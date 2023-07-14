6a599fe9746c8c3b1380fa7f997cd8e82389e83f
KingEddie01 <edk6004@gmail.com>
2023-07-14 19:37:51 +0100

    Simple Prcatice Applications

---
+++ Arraysnight.java
@@ -0,0 +1,25 @@
+public class Arraysnight {
+    public static void main(String[] args) {
+
+    int[] numbers = new int[5];
+    numbers[0] = 2;
+    numbers[1] = 4;
+    numbers[2] = 7;
+    number[3] = 15;
+    number[4] = 9
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
